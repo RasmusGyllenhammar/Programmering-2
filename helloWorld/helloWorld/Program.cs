e9d7797c9282c220bc23a97a52d844fd3a9d2af6
RasmusGyllenhammar <rasmus.gyllenhammar@elev.ga.ntig.se>
2020-09-02 21:26:06 +0200

    switch case

--- helloWorld/helloWorld/Program.cs
+++ helloWorld/helloWorld/Program.cs
@@ -9,8 +9,6 @@ namespace helloWorld
             Console.WriteLine("Hello Niklas!");
 
             string yesOrNo = "";
-
-            bool alive = true;
             
             Console.WriteLine("write your age, type 'yes to be alive or write 'no' to be ghost and do not forget your name ");
 
@@ -18,20 +16,57 @@ namespace helloWorld
 
             var inputAge = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("oh too young, yet too old " + inputAge);
+            switch (inputAge)
+            {
+                case 15:
+                    Console.WriteLine("MADT only 15");
+                    break;
+                case 16:
+                    Console.WriteLine("sweet 16 EYYYY");
+                    break;
+                case 17:
+                    Console.WriteLine("omg SOON 18 O:");
+                    break;
+
+                case 18:
+                    Console.WriteLine("omg 18 you can get driver license");
+                    break;
+
+                default:
+                    Console.WriteLine("that age is very unusual huh " + inputAge);
+                    break;
+            }
+
+   
 
 
-            yesOrNo = Console.ReadLine();
-                
-                if(yesOrNo == "yes")
+
+
+
+           
+            var questionToBeAlive = true;
+            while (questionToBeAlive)
+            { yesOrNo = Console.ReadLine();
+                if (yesOrNo == "yes")
                 {
                     Console.WriteLine("omg you ARE ALIVE");
+                    break;
                 }
-                else if(yesOrNo == "no")
+                else if (yesOrNo == "no")
                 {
                     Console.WriteLine("OH LORD JESUS YOU ARE DED");
-                    
+                    break;
+
                 }
+                else
+                {
+                    Console.WriteLine("are you alive or dead?");
+                    continue;
+                }
+            }
+               
+            
+            
             var test = true;
             while (test)
             {
